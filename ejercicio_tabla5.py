2f2c079da1c5dafae360c4e56cbb6af95fabee2c
alejandro1201 <102228998+alejandro1201@users.noreply.github.com>
2022-05-06 06:50:02 -0500

    Update ejercicio_tabla5.py

--- ejercicio_tabla5.py
+++ ejercicio_tabla5.py
@@ -1,9 +1,6 @@
-
-
-lista=['one','two']
-lista1= list(map(str.upper,lista))
-print(lista1)
-# for i in lista:
-#     if lista:
-#         lista[i].upper()
-# print(i)
+p=input('Digite la cadena: ')
+p1=p.split()
+p1=list(map(str.upper,p1))
+print(p1)
+sinduplicado=set(p1)
+print(sinduplicado)
